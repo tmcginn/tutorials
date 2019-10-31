3f77ad8c2bbdda4fa8d552a08c33e86a23bad151
Ashwin Agarwal <ashwiaga@oradev.oraclecorp.com>
2019-10-31 13:19:20 +0530

    Updated files

--- tutorial_creator/js/main.js
+++ tutorial_creator/js/main.js
@@ -161,7 +161,8 @@ function homeInit() {
 }
 
 function manifestInit() {
-    setFormData();        
+    setFormData();     
+    getFormData();   
 }
 function loadFile(filename) {
     var xhr = new XMLHttpRequest();
@@ -208,7 +209,8 @@ function setFormData() {
 
     $.each(data, function (i) {
         for (key in data[i]) {
-            $('input[name="' + key + '"]:eq(' + i + ')').val($.trim(data[i][key]));
+            console.log(key) + " " + data[i][key];
+            $('input[name="' + key + '"]:eq(' + i + '), textarea[name="' + key + '"]:eq(' + i + ')').val($.trim(data[i][key]));
         }
     });
 }
