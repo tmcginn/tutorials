4a1e11b373d76741c827b2b13a96a45de53db756
Ashwin Agarwal <ashwiaga@oradev.oraclecorp.com>
2019-11-08 18:50:17 +0530

    Updated files

--- tutorial_creator/preview/preview.js
+++ tutorial_creator/preview/preview.js
@@ -13,7 +13,7 @@ $(function () {
 			prepareMd(markdownContent, tutorialEntryInManifest, articleElement);
 		}).done(function () { //do the following after all the above operations are complete
 			showMd(articleElement);
-		}).fail(function() {
+		}).fail(function () {
 			alert("File does not exist at location: " + tutorialEntryInManifest.filename);
 		});
 	}
@@ -22,70 +22,6 @@ $(function () {
 		showMd(articleElement);
 	}
 });
-
-function downloadZip() {
-	var previewType = window.localStorage.getItem("preview");
-	if (previewType !== "manifest") { return; }
-	var localStorageManifest = JSON.parse(window.localStorage.getItem("manifestValue"));
-	var allTutorials = JSON.parse(localStorageManifest).tutorials;
-	var tutorialHtml = [];
-
-	$.get("https://raw.githubusercontent.com/ashwin-agarwal/tutorials/master/template/download.html", function (htmlTemplate) {
-		$(allTutorials).each(function (i, tutorialEntryInManifest) {
-			$.get(tutorialEntryInManifest.filename, function (markdownContent) { //reading MD file in the manifest and storing content in markdownContent variable
-				var articleElement = document.createElement('article');
-				$(articleElement).html(new showdown.Converter().makeHtml(markdownContent)); //converting markdownContent to HTML by using showndown plugin				
-				addPathToImageSrc(articleElement, tutorialEntryInManifest.filename); //adds the path for the image based on the filename in manifest				
-				wrapSectionTagAndAddHorizonatalLine(articleElement); //adding each section within section tag and adding HR line
-				addH2ImageIcons(articleElement); //Adding image, class, width, and height to the h2 title img
-				wrapImgWithFigure(articleElement); //Wrapping images with figure, adding figcaption to all those images that have title in the MD
-				fixFigCaptions(articleElement, tutorialEntryInManifest.filename); //Fixing figcaptions for those images that were loaded from the localstorage as the src of the localstorage is like a junk value
-				addPathToAllRelativeHref(articleElement, tutorialEntryInManifest.filename); //adding the path for all HREFs that are relative based on the filename in manifest
-				movePreInsideLi(articleElement); //moving the pre elements a layer up for stylesheet matching
-				$(articleElement).find('a').attr('target', '_blank'); //setting target for all ahrefs to _blank	
-				$(articleElement).find('ul li p:first-child').contents().unwrap(); //removing the p tag from first li child as CSS changes the formatting											
-
-				tutorialHtml.push(document.implementation.createHTMLDocument());				
-				var tmpElement = document.createElement('html');
-				tmpElement.innerHTML = htmlTemplate;				
-				tutorialHtml[i].head.innerHTML = $($(tmpElement).find('head')[0]).html();
-				tutorialHtml[i].body.innerHTML = $($(tmpElement).find('body')[0]).html();
-				console.log(tutorialHtml[i]);
-				//$(tutorialHtml[i]).attr('lang', 'en');
-				$(tutorialHtml[i]).find('#bookContainer').html(articleElement);
-			}).done(function () { //do the following after all the above operations are complete
-				//updateh1Title function
-				var articleH1Title = $(tutorialHtml[i]).find('article>h1').text();
-				var templateH1Title = $(tutorialHtml[i]).find("#content>h1").text();
-				var replacedH1Html = $(tutorialHtml[i]).find("#content>h1").html().replace(templateH1Title, articleH1Title);
-				$(tutorialHtml[i]).find("#content>h1").html(replacedH1Html);
-				$(tutorialHtml[i]).find('article>h1').remove();
-
-				//update head content
-				$(tutorialHtml[i]).find('title').text(tutorialEntryInManifest.title);
-				$(tutorialHtml[i]).find('meta[name=contentid]').attr("content", tutorialEntryInManifest.contentid);
-				$(tutorialHtml[i]).find('meta[name=description]').attr("content", tutorialEntryInManifest.description);
-				$(tutorialHtml[i]).find('meta[name=partnumber]').attr("content", tutorialEntryInManifest.partnumber);
-				$(tutorialHtml[i]).find('meta[name=publisheddate]').attr("content", tutorialEntryInManifest.publisheddate);
-
-				download("index.html", "<!DOCTYPE html>" + tutorialHtml[i].documentElement.outerHTML);
-			});
-		});
-	});
-}
-
-function download(filename, text) {
-	var pom = document.createElement('a');
-	pom.setAttribute('href', 'data:html/plain;charset=utf-8,' + encodeURIComponent(text));
-	pom.setAttribute('download', filename);
-	if (document.createEvent) {
-		var event = document.createEvent('MouseEvents');
-		event.initEvent('click', true, true);
-		pom.dispatchEvent(event);
-	} else {
-		pom.click();
-	}
-}
 
 function prepareMd(markdownContent, tutorialEntryInManifest, articleElement) {
 	var previewType = window.localStorage.getItem("preview");
